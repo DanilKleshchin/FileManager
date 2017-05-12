96fa56b3baeb977075b180b786bd18fb148836b4
danil.kleshchin <danil.kleshchin@gmail.com>
2017-05-12 13:48:09 +0300

    Implement back button

--- app/src/main/java/com/kleshchin/danil/filemanager/MainActivity.java
+++ app/src/main/java/com/kleshchin/danil/filemanager/MainActivity.java
@@ -9,131 +9,141 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
-    ListAdapter listAdapter;
-    ArrayList<ListItems> listItems;
-    ArrayList<ListItems> listFilesItems;
-    Toolbar toolbar;
-    String mainPath = "/storage";
-    String currentDirectory;
+    private ListView listView;
+    private ListAdapter listAdapter;
+    private ArrayList<ListItemStructure> listItems = new ArrayList<>();
+    private String mainPath = "/storage";
+    private String currentDirectory;
+    EditText toolbarTitle;
+    public static final String LISTVIEW_STATE = "listview.state";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listView);
-        listItems = new ArrayList<>();
-        listFilesItems = new ArrayList<>();
-        listAdapter = new ListAdapter(getApplicationContext(), listItems);
+        listAdapter = new ListAdapter(this, listItems);
+        toolbarTitle = (EditText) findViewById(R.id.toolbar_title);
         initToolbar();
         fillListView(new File(mainPath));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String path = ((EditText) findViewById(R.id.toolbar_title)).getText().toString() +
-                        "/" + listItems.get(i).fileName;
+                String path = toolbarTitle.getText().toString() + "/" + listItems.get(i).getFileName();
                 if (new File(path).isDirectory()) {
                     fillListView(new File(path));
                 } else {
-                    Uri uri = Uri.fromFile(new File(path));
-                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
-                    String mime = "*/*";
-                    MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-                    if (mimeTypeMap.hasExtension(
-                            mimeTypeMap.getFileExtensionFromUrl(uri.toString())))
-                        mime = mimeTypeMap.getMimeTypeFromExtension(
-                                mimeTypeMap.getFileExtensionFromUrl(uri.toString()));
-                    intent.setDataAndType(uri, mime);
-                    startActivity(intent);
+                    onFileSelected(path);
                 }
             }
         });
     }
 
+    private void onFileSelected(String path) {
+        Uri uri = Uri.fromFile(new File(path));
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        String mime = "*/*";
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        if (mimeTypeMap.hasExtension(
+                mimeTypeMap.getFileExtensionFromUrl(uri.toString())))
+            mime = mimeTypeMap.getMimeTypeFromExtension(
+                    mimeTypeMap.getFileExtensionFromUrl(uri.toString()));
+        intent.setDataAndType(uri, mime);
+        startActivity(intent);
+    }
+
     private void initToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        toolbar.setLogo(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
-        final EditText toolbarText = (EditText) findViewById(R.id.toolbar_title);
-        //toolbarText.setLines(1);
-        if (!toolbarText.getText().equals("") &&
-                !toolbarText.getText().equals(mainPath)) {
-            final Drawable upArrow = getResources().getDrawable(R.drawable.ic_arrow_back_black_36dp);
-            upArrow.setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_ATOP);
-            toolbar.setLogo(upArrow);
-            setLogoOnClickListener(toolbar, new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    fillListView(new File(toolbarText.getText().
-                            toString().substring(0, toolbarText.
-                            getText().toString().length() - 1 - currentDirectory.length() + 1)));
-                }
-            });
+        final Drawable upArrow = getResources().getDrawable(R.drawable.ic_arrow_back_black_36dp);
+        upArrow.setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_ATOP);
+        String toolbarText = toolbarTitle.getText().toString();
+        if (!toolbarText.equals("") && !toolbarText.equals(mainPath)) {
+            getSupportActionBar().setHomeAsUpIndicator(upArrow);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        } else {
+            getSupportActionBar().setHomeButtonEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowHomeEnabled(false);
         }
     }
 
-    private void setLogoOnClickListener(Toolbar toolbar, View.OnClickListener listener) {
-        try {
-            Class<?> toolbarClass = Toolbar.class;
-            Field logoField = toolbarClass.getDeclaredField("mLogoView");
-            logoField.setAccessible(true);
-            ImageView logoView = (ImageView) logoField.get(toolbar);
-            if (logoView != null) {
-                logoView.setOnClickListener(listener);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                String toolbarText = toolbarTitle.getText().toString();
+                fillListView(new File(toolbarText.substring(0,
+                        toolbarText.length() - 1 - currentDirectory.length() + 1)));
+                break;
         }
+
+        return true;
     }
 
     private void fillListView(File file) {
         listItems.clear();
-        listFilesItems.clear();
-        listView.setAdapter(listAdapter);
         File list[] = file.listFiles();
-        ((EditText) findViewById(R.id.toolbar_title)).setText(file.getPath());
+        toolbarTitle.setText(file.getPath());
+        toolbarTitle.setSelection(toolbarTitle.getText().length());
         initToolbar();
         currentDirectory = file.getName();
         if (list != null) {
             for (File currentFile : list) {
                 if (currentFile.isDirectory()) {
-                    listItems.add(new ListItems(currentFile.getName(), R.drawable.folder));
+                    listItems.add(new ListItemStructure(currentFile.getName(), R.drawable.folder));
                 } else {
-                    listFilesItems.add(new ListItems(currentFile.getName(), R.drawable.file));
+                    listItems.add(new ListItemStructure(currentFile.getName(), R.drawable.file));
                 }
             }
-            listItems.addAll(listFilesItems);
+            Collections.sort(listItems, new Comparator<ListItemStructure>() {
+
+                @Override
+                public int compare(ListItemStructure l1, ListItemStructure l2) {
+                    int nameCompare = String.valueOf(l2.getImageName())
+                            .compareToIgnoreCase(String.valueOf(l1.getImageName()));
+                    if (nameCompare != 0) {
+                        return nameCompare;
+                    } else {
+                        return l1.getFileName().compareTo(l2.getFileName());
+                    }
+                }
+            });
             listView.setAdapter(listAdapter);
         }
     }
 
+
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        listView.setAdapter(listAdapter);
-        outState.putParcelable("listview.state", listView.onSaveInstanceState());
+        outState.putParcelable(LISTVIEW_STATE, listView.onSaveInstanceState());
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         initToolbar();
-        Parcelable listViewState = savedInstanceState.getParcelable("listview.state");
+        Parcelable listViewState = savedInstanceState.getParcelable(LISTVIEW_STATE);
         listView.onRestoreInstanceState(listViewState);
     }
 }
