e612cd8c64115813ceed2f4e160ebbad0f82e09e
Danil Kleshchin <danil.kleshchin@gmail.com>
2017-06-26 12:11:40 +0300

    Create insertion method

--- app/src/main/java/com/kleshchin/danil/filemanager/DBHelper.java
+++ app/src/main/java/com/kleshchin/danil/filemanager/DBHelper.java
@@ -1,49 +1,65 @@
 package com.kleshchin.danil.filemanager;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
 
 /**
  * Created by Danil Kleshchin on 22.06.2017.
  */
 
-public class DBHelper extends SQLiteOpenHelper{
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "File_Manager";
-    public static final String TABLE_NAME = "";
-    public static final String KEY_ID = "_id";
-    public static final String KEY_FILE_PATH = "File_path";
-    public static final String KEY_FILE_SIZE = "File_size";
-    public static final String KEY_FILE_MODIFIED_DATE = "File_modified_date";
-    public static final String KEY_FILE_IMAGE = "File_image";
+class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "File_Manager";
+    static final String TABLE_NAME = "File_data";
+    private static final String KEY_ID = "_id";
+    static final String KEY_FILE_PATH = "File_path";
+    static final String KEY_FILE_SIZE = "File_size";
+    static final String KEY_FILE_MODIFIED_DATE = "File_modified_date";
 
     private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
+    private static final String FILE_PATH_INDEX = "file_path_index";
 
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE" + TABLE_NAME + " (" +
-            KEY_ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
-            KEY_FILE_PATH + TEXT_TYPE + COMMA_SEP +
-            KEY_FILE_SIZE + TEXT_TYPE + COMMA_SEP +
-            KEY_FILE_MODIFIED_DATE + TEXT_TYPE + COMMA_SEP +
-            KEY_FILE_IMAGE + TEXT_TYPE + COMMA_SEP + " )";
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    KEY_ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+                    KEY_FILE_PATH + TEXT_TYPE + COMMA_SEP +
+                    KEY_FILE_SIZE + INT_TYPE + COMMA_SEP +
+                    KEY_FILE_MODIFIED_DATE + INT_TYPE + COMMA_SEP +
+                    "UNIQUE" + "(" + KEY_FILE_PATH + ")" + "ON CONFLICT REPLACE" + ")";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
 
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, null, version);
+    private static final String SQL_CREATE_INDEX =
+            "CREATE INDEX " + FILE_PATH_INDEX + " ON " + TABLE_NAME + "(" + KEY_FILE_PATH + ")";
+
+    DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_CREATE_INDEX);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
+    }
+
+    void insertIntoDB(Long size, File file, SQLiteDatabase database) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBHelper.KEY_FILE_SIZE, size);
+        contentValues.put(DBHelper.KEY_FILE_MODIFIED_DATE, file.lastModified());
+        contentValues.put(DBHelper.KEY_FILE_PATH, file.getPath());
+        database.insert(DBHelper.TABLE_NAME, null, contentValues);
     }
 }
